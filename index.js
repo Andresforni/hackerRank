527b7891c3eafde485162dee2a1f538fca3a4aed
Andresforni <123376922+Andresforni@users.noreply.github.com>
2023-02-05 11:33:42 -0300

    1 file modified

--- index.js
+++ index.js
@@ -1,33 +1,41 @@
-let s = [[5,3,4], 
-         [1,5,8],
-         [6,4,2]];
-//let s = [[8, 3, 4], [1, 5, 9], [6, 7, 2]];
+let s = [
+  [5, 3, 4],
+  [1, 5, 8],
+  [6, 4, 2],
+];
+s = [
+  [8, 3, 4],
+  [1, 5, 9],
+  [6, 7, 2],
+];
 let m = 3;
-result=[];
-
+result = [];
 
 console.log(isMagic(s));
 console.log(result);
 
-
-function isMagic(s){
-
-  let resultTemp = [];
-  for(let i=0; i<m; i++){
-    let sumValue=0;
-    let sumValue1=0;
-    for(let j=0; j<m; j++){
-      sumValue+=s[i][j];
-      sumValue1+=s[j][i];
+function isMagic(s) {
+  result1 = [];
+  let magic = true;
+  for (let i = 0; i < m; i++) {
+    let sumValue = 0;
+    let sumValue1 = 0;
+    for (let j = 0; j < m; j++) {
+      sumValue += s[i][j];
+      sumValue1 += s[j][i];
     }
     result.push(sumValue);
-    resultTemp.push(sumValue1);
+    if (sumValue1 != 15) magic = false;
+    result1.push(sumValue1);
+    if (sumValue1 != 15) magic = false;
   }
-  result=result.concat(resultTemp);
-  return true;
+  result = result.concat(result1);
+  let sum = s[0][0] + s[1][1] + s[2][2];
+  result.push(sum);
+  if (sum != 15) magic = false;
+  sum = s[0][2] + s[1][1] + s[2][0];
+  result.push(sum);
+  if (sum != 15) magic = false;
+
+  return magic;
 }
-
-
-
-
-
