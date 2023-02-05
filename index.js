a1cafaf1824e910093872b47103f6ded0cb82078
Andresforni <123376922+Andresforni@users.noreply.github.com>
2023-02-05 11:58:07 -0300

    1 file modified

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
-let s = [
+let s = [ // 7
   [5, 3, 4],
   [1, 5, 8],
   [6, 4, 2],
 ];
-s = [
-  [8, 3, 4],
-  [1, 5, 9],
-  [6, 7, 2],
-];
+// s = [
+//   [4, 8, 2],
+//   [4, 5, 7], 
+//   [6, 1, 6]]; //no
+// x
+
+
+
+// si no estan todos los numero se divide por dos
+//si estan todos no se divide por dos
+
+
+// s = [
+//   [8, 3, 4],
+//   [1, 5, 9],
+//   [6, 7, 2],
+// ];
 let m = 3;
 result = [];
 
 console.log(isMagic(s));
 console.log(result);
+console.log(sume(s));
 
 function isMagic(s) {
   result1 = [];
@@ -38,4 +51,13 @@ function isMagic(s) {
   if (sum != 15) magic = false;
 
   return magic;
+}
+
+
+function sume(s){
+  let sum =0;
+  result.forEach(item=>{
+    sum+=Math.abs(15-item);
+  });
+return sum;
 }
